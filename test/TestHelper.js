49d7cfdda010b11d379818859ed322a5c4e8f94f
Niklas Kiefer <niklas.kiefer@camunda.com>
2021-01-29 11:47:00 +0100

    feat: add modeling module

--- test/TestHelper.js
+++ test/TestHelper.js
@@ -1,6 +1,13 @@
 export * from 'bpmn-js/test/helper';
 
 import {
+  attr as domAttr,
+  query as domQuery,
+  queryAll as domQueryAll
+
+} from 'min-dom';
+
+import {
   bootstrapBpmnJS,
   insertCSS
 } from 'bpmn-js/test/helper';
@@ -19,4 +26,145 @@ insertCSS(
 
 export function bootstrapZeebeModeler(diagram, options, locals) {
   return bootstrapBpmnJS(Modeler, diagram, options, locals);
+}
+
+/**
+ * Triggers a change event
+ *
+ * @param element on which the change should be triggered
+ * @param eventType type of the event (e.g. click, change, ...)
+ */
+export function triggerEvent(element, eventType) {
+
+  let evt;
+
+  eventType = eventType || 'change';
+
+  if (document.createEvent) {
+    try {
+
+      // Chrome, Safari, Firefox
+      evt = new MouseEvent((eventType), {
+        view: window,
+        bubbles: true,
+        cancelable: true
+      });
+    } catch (e) {
+
+      // IE 11, PhantomJS (wat!)
+      evt = document.createEvent('MouseEvent');
+
+      evt.initEvent((eventType), true, true);
+    }
+    return element.dispatchEvent(evt);
+  } else {
+
+    // Welcome IE
+    evt = document.createEventObject();
+
+    return element.fireEvent('on' + eventType, evt);
+  }
+}
+
+/**
+ * Set the new value to the given element.
+ *
+ * @param element on which the change should be triggered
+ * @param value new value for the element
+ * @param eventType (optional) type of the event (e.g. click, change, ...)
+ * @param cursorPosition (optional) position ot the cursor after changing
+ *                                  the value
+ */
+export function triggerValue(element, value, eventType, cursorPosition) {
+  if (typeof eventType == 'number') {
+    cursorPosition = eventType;
+    eventType = null;
+  }
+
+  element.focus();
+
+  if (domAttr(element, 'contenteditable')) {
+    element.innerText = value;
+  } else {
+    element.value = value;
+  }
+
+  if (cursorPosition) {
+    element.selectionStart = cursorPosition;
+    element.selectionEnd = cursorPosition;
+  }
+
+  triggerEvent(element, eventType);
+}
+
+export function triggerInput(element, value) {
+  element.value = value;
+
+  triggerEvent(element, 'input');
+
+  element.focus();
+}
+
+export function triggerKeyEvent(element, event, code) {
+  let e = document.createEvent('Events');
+
+  if (e.initEvent) {
+    e.initEvent(event, true, true);
+  }
+
+  e.keyCode = code;
+  e.which = code;
+
+  element.dispatchEvent(e);
+}
+
+
+/**
+ * Select a form field with the specified index in the DOM
+ *
+ * @param  {number} index
+ * @param  {DOMElement} container
+ */
+export function triggerFormFieldSelection(index, container) {
+  let formFieldSelectBox = domQuery(
+    'select[name=selectedExtensionElement]',
+    container
+  );
+
+  formFieldSelectBox.options[index].selected = 'selected';
+  triggerEvent(formFieldSelectBox, 'change');
+}
+
+/**
+ *  Select the option with the given value
+ *
+ *  @param element contains the options
+ *  @param optionValue value which should be selected
+ */
+export function selectedByOption(element, optionValue) {
+
+  const options = domQueryAll('option', element);
+
+  for (let i = 0; i< options.length; i++) {
+
+    const option = options[i];
+
+    if (option.value === optionValue) {
+      element.selectedIndex = i;
+      break;
+    }
+  }
+}
+
+/**
+ * PhantomJS Speciality
+ * @param element
+ * @returns {*}
+ */
+export function selectedByIndex(element) {
+  if (!element) {
+    return null;
+  }
+
+  return element.options[element.selectedIndex];
 }
---
+++ test/features/modeling/UpdatePropagateAllChildVariablesBehaviorIntegrationSpec.js
@@ -0,0 +1,375 @@
+import {
+  bootstrapZeebeModeler,
+  inject,
+  triggerEvent
+} from '../../TestHelper';
+
+import {
+  getOutputParameters,
+  getInputOutput
+} from '../../../lib/helper/InputOutputHelper';
+
+import {
+  getCalledElement
+} from '../../../lib/helper/CalledElementHelper';
+
+import TestContainer from 'mocha-test-container-support';
+
+import {
+  query as domQuery
+} from 'min-dom';
+
+import coreModule from 'bpmn-js/lib/core';
+import modelingModule from 'bpmn-js/lib/features/modeling';
+import propertiesPanelModule from 'bpmn-js-properties-panel';
+import propertiesProviderModule from '../../../lib/features/properties-provider';
+import selectionModule from 'diagram-js/lib/features/selection';
+import zeebeModdleExtensions from 'zeebe-bpmn-moddle/resources/zeebe';
+
+import zeebeModelingModules from '../../../lib/features/modeling';
+
+import diagramXML from './process-call-activities.bpmn';
+
+
+describe('features/modeling/behavior integrationTest - update propagateAllChildVariables attribute on call activities', function() {
+
+  const testModules = [
+    coreModule,
+    modelingModule,
+    propertiesPanelModule,
+    propertiesProviderModule,
+    selectionModule,
+    zeebeModelingModules
+  ];
+
+  const moddleExtensions = {
+    zeebe: zeebeModdleExtensions
+  };
+
+  let container;
+
+  beforeEach(function() {
+    container = TestContainer.get(this);
+  });
+
+  beforeEach(bootstrapZeebeModeler(diagramXML, {
+    modules: testModules,
+    moddleExtensions
+  }));
+
+  beforeEach(inject(function(propertiesPanel) {
+    propertiesPanel.attachTo(container);
+  }));
+
+
+  describe('remove outputParameters', function() {
+
+
+    describe('when toggling on with outputParameters and inputParameters', function() {
+
+      let shape;
+
+
+      describe('<propagateAllChildVariables> explicitly set', function() {
+
+        beforeEach(inject(function(selection, elementRegistry) {
+
+          // given
+          shape = elementRegistry.get('CallActivity_3');
+
+          // when
+          selection.select(shape);
+          clickPropagateAllChildVariablesToggle(container);
+        }));
+
+
+        it('should execute', inject(function() {
+
+          // then
+          const outputParameters = getOutputParameters(shape);
+          expect(outputParameters.length).to.equal(0);
+        }));
+
+
+        it('should undo', inject(function(commandStack) {
+
+          // when
+          commandStack.undo();
+
+          // then
+          const outputParameters = getOutputParameters(shape);
+          expect(outputParameters).to.exist;
+          expect(outputParameters.length).to.equal(1);
+        }));
+
+
+        it('should undo/redo', inject(function(commandStack) {
+
+          // when
+          commandStack.undo();
+          commandStack.redo();
+
+          // then
+          const outputParameters = getOutputParameters(shape);
+          expect(outputParameters.length).to.equal(0);
+        }));
+
+      });
+
+
+      describe('<propagateAllChildVariables> not explicitly set (legacy callActivity)', function() {
+
+        beforeEach(inject(function(selection, elementRegistry) {
+
+          // given
+          shape = elementRegistry.get('CallActivity_4');
+
+          // when
+          selection.select(shape);
+          clickPropagateAllChildVariablesToggle(container);
+        }));
+
+
+        it('should execute', inject(function() {
+
+          // then
+          const outputParameters = getOutputParameters(shape);
+          expect(outputParameters.length).to.equal(0);
+        }));
+
+
+        it('should undo', inject(function(commandStack) {
+
+          // when
+          commandStack.undo();
+
+          // then
+          const outputParameters = getOutputParameters(shape);
+          expect(outputParameters).to.exist;
+          expect(outputParameters.length).to.equal(1);
+        }));
+
+
+        it('should undo/redo', inject(function(commandStack) {
+
+          // when
+          commandStack.undo();
+          commandStack.redo();
+
+          // then
+          const outputParameters = getOutputParameters(shape);
+          expect(outputParameters.length).to.equal(0);
+        }));
+
+      });
+
+    });
+
+  });
+
+
+  describe('remove iOMapping', function() {
+
+
+    describe('when toggling on with outputParameters', function() {
+
+      let shape;
+
+
+      describe('<propagateAllChildVariables> explicitly set', function() {
+
+        beforeEach(inject(function(selection, elementRegistry) {
+
+          // given
+          shape = elementRegistry.get('CallActivity_5');
+
+          // when
+          selection.select(shape);
+          clickPropagateAllChildVariablesToggle(container);
+        }));
+
+
+        it('should execute', inject(function() {
+
+          // then
+          const inputOutput = getInputOutput(shape);
+          expect(inputOutput).to.not.exist;
+        }));
+
+
+        it('should undo', inject(function(commandStack) {
+
+          // when
+          commandStack.undo();
+
+          // then
+          const outputParameters = getOutputParameters(shape);
+          expect(outputParameters).to.exist;
+          expect(outputParameters.length).to.equal(1);
+
+          const inputOutput = getInputOutput(shape);
+          expect(inputOutput).to.exist;
+        }));
+
+
+        it('should undo/redo', inject(function(commandStack) {
+
+          // when
+          commandStack.undo();
+          commandStack.redo();
+
+          // then
+          const inputOutput = getInputOutput(shape);
+          expect(inputOutput).to.not.exist;
+        }));
+
+      });
+
+
+      describe('<propagateAllChildVariables> not explicitly set (legacy callActivity)', function() {
+
+        beforeEach(inject(function(selection, elementRegistry) {
+
+          // given
+          shape = elementRegistry.get('CallActivity_6');
+
+          // when
+          selection.select(shape);
+          clickPropagateAllChildVariablesToggle(container);
+        }));
+
+
+        it('should execute', inject(function() {
+
+          // then
+          const inputOutput = getInputOutput(shape);
+          expect(inputOutput).to.not.exist;
+        }));
+
+
+        it('should undo', inject(function(commandStack) {
+
+          // when
+          commandStack.undo();
+
+          // then
+          const outputParameters = getOutputParameters(shape);
+          expect(outputParameters).to.exist;
+          expect(outputParameters.length).to.equal(1);
+
+          const inputOutput = getInputOutput(shape);
+          expect(inputOutput).to.exist;
+        }));
+
+
+        it('should undo/redo', inject(function(commandStack) {
+
+          // when
+          commandStack.undo();
+          commandStack.redo();
+
+          // then
+          const inputOutput = getInputOutput(shape);
+          expect(inputOutput).to.not.exist;
+        }));
+
+      });
+
+    });
+
+
+  });
+
+
+  describe('set propagateAllChildVariables to false', function() {
+
+
+    describe('when adding output parameters', function() {
+
+      let shape, calledElement;
+
+      beforeEach(inject(function(selection, elementRegistry) {
+
+        // given
+        shape = elementRegistry.get('CallActivity_7');
+        calledElement = getCalledElement(shape);
+
+        // assume
+        const inputOutput = getInputOutput(shape);
+        expect(inputOutput).to.not.exist;
+
+        // when
+        selection.select(shape);
+        clickAddOutputParameterButton(container);
+      }));
+
+
+      it('should execute', inject(function() {
+
+        // then
+        expect(calledElement.propagateAllChildVariables).to.equal(false);
+      }));
+
+
+      it('should undo', inject(function(commandStack) {
+
+        // when
+        commandStack.undo();
+
+        // assume
+        expect(calledElement.propagateAllChildVariables).to.equal(true);
+      }));
+
+
+      it('should undo/redo', inject(function(commandStack) {
+
+        // when
+        commandStack.undo();
+        commandStack.redo();
+
+        // then
+        expect(calledElement.propagateAllChildVariables).to.equal(false);
+      }));
+
+    });
+
+  });
+
+});
+
+
+// helper /////////
+
+const getPropagateAllChildVariablesToggle = (container) => {
+  return domQuery('#output-propagate-all-toggle', container);
+};
+
+const clickPropagateAllChildVariablesToggle = (container) => {
+  const toggle = getPropagateAllChildVariablesToggle(container);
+  triggerEvent(toggle, 'click');
+};
+
+const getAddButton = (container) => {
+  return domQuery('button[data-action="createElement"].bpp-input-output__add', container);
+};
+
+const getAddOutputParameterButton = (container) => {
+  return getAddButton(getOutputParameterGroup(container));
+};
+
+const clickAddOutputParameterButton = (container) => {
+  const addButton = getAddOutputParameterButton(container);
+  triggerEvent(addButton, 'click');
+};
+
+const getInputOutputTab = (container) => {
+  return domQuery('div[data-tab="input-output"]', container);
+};
+
+const getParameterGroup = (type, container) => {
+  return domQuery(`div[data-group="${type}"]`, getInputOutputTab(container));
+};
+
+const getOutputParameterGroup = (container) => {
+  return getParameterGroup('output', container);
+};
