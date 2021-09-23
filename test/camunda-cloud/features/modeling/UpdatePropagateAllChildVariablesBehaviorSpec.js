c106c41a85112e97784a7d55c16aa97baeab2707
Philipp <philippfromme@outlook.com>
2021-09-23 09:31:55 +0200

    fix(cloud): do not monkey patch and refactor tests

--- test/camunda-cloud/features/modeling/UpdatePropagateAllChildVariablesBehaviorSpec.js
+++ test/camunda-cloud/features/modeling/UpdatePropagateAllChildVariablesBehaviorSpec.js
@@ -12,87 +12,67 @@ import {
   getCalledElement
 } from 'lib/camunda-cloud/helper/CalledElementHelper';
 
-import coreModule from 'bpmn-js/lib/core';
-
-import modelingModule from 'bpmn-js/lib/features/modeling';
-
-import zeebeModdleExtensions from 'zeebe-bpmn-moddle/resources/zeebe';
-
-import zeebeModelingModules from 'lib/camunda-cloud/features/modeling';
+import { getBusinessObject } from 'bpmn-js/lib/util/ModelUtil';
 
 import diagramXML from './process-call-activities.bpmn';
 
 
+
 describe('camunda-cloud/features/modeling - update propagateAllChildVariables attribute on call activities', function() {
 
-
-  const testModules = [
-    coreModule,
-    modelingModule,
-    zeebeModelingModules
-  ];
-
-  const moddleExtensions = {
-    zeebe: zeebeModdleExtensions
-  };
-
-  beforeEach(bootstrapCamundaCloudModeler(diagramXML, {
-    modules: testModules,
-    moddleExtensions
-  }));
+  beforeEach(bootstrapCamundaCloudModeler(diagramXML));
 
 
   describe('remove outputParameters', function() {
 
-    let shape, context;
+    let element;
 
-    beforeEach(inject(function(elementRegistry, eventBus) {
+    beforeEach(inject(function(commandStack, elementRegistry) {
 
       // given
-      shape = elementRegistry.get('CallActivity_3');
+      element = elementRegistry.get('CallActivity_3');
 
-      context = {
-        context: {
-          element: shape,
-          properties: {
-            propagateAllChildVariables: true
-          }
-        }
-      };
+      const businessObject = getBusinessObject(element);
 
       // when
-      eventBus.fire('commandStack.properties-panel.update-businessobject.executed', context);
+      commandStack.execute('properties-panel.update-businessobject', {
+        businessObject,
+        element,
+        properties: {
+          propagateAllChildVariables: true
+        }
+      });
     }));
 
 
     it('should execute', inject(function() {
 
       // then
-      const outputParameters = getOutputParameters(shape);
+      const outputParameters = getOutputParameters(element);
       expect(outputParameters.length).to.equal(0);
     }));
 
 
-    it('should undo', inject(function(eventBus) {
+    it('should undo', inject(function(commandStack) {
 
       // when
-      eventBus.fire('commandStack.properties-panel.update-businessobject.reverted', context);
+      commandStack.undo();
 
       // then
-      const outputParameters = getOutputParameters(shape);
+      const outputParameters = getOutputParameters(element);
       expect(outputParameters).to.exist;
       expect(outputParameters.length).to.equal(1);
     }));
 
 
-    it('should undo/redo', inject(function(eventBus) {
+    it('should undo/redo', inject(function(commandStack) {
 
       // when
-      eventBus.fire('commandStack.properties-panel.update-businessobject.reverted', context);
-      eventBus.fire('commandStack.properties-panel.update-businessobject.executed', context);
+      commandStack.undo();
+      commandStack.redo();
 
       // then
-      const outputParameters = getOutputParameters(shape);
+      const outputParameters = getOutputParameters(element);
       expect(outputParameters.length).to.equal(0);
     }));
 
@@ -101,58 +81,57 @@ describe('camunda-cloud/features/modeling - update propagateAllChildVariables at
 
   describe('remove iOMapping', function() {
 
-    let shape, context;
+    let element;
 
-    beforeEach(inject(function(eventBus, elementRegistry) {
+    beforeEach(inject(function(commandStack, elementRegistry) {
 
       // given
-      shape = elementRegistry.get('CallActivity_5');
+      element = elementRegistry.get('CallActivity_5');
 
-      context = {
-        context: {
-          element: shape,
-          properties: {
-            propagateAllChildVariables: true
-          }
-        }
-      };
+      const businessObject = getBusinessObject(element);
 
       // when
-      eventBus.fire('commandStack.properties-panel.update-businessobject.executed', context);
+      commandStack.execute('properties-panel.update-businessobject', {
+        businessObject,
+        element,
+        properties: {
+          propagateAllChildVariables: true
+        }
+      });
     }));
 
 
     it('should execute', inject(function() {
 
       // then
-      const inputOutput = getInputOutput(shape);
+      const inputOutput = getInputOutput(element);
       expect(inputOutput).to.not.exist;
     }));
 
 
-    it('should undo', inject(function(eventBus) {
+    it('should undo', inject(function(commandStack) {
 
       // when
-      eventBus.fire('commandStack.properties-panel.update-businessobject.reverted', context);
+      commandStack.undo();
 
       // then
-      const outputParameters = getOutputParameters(shape);
+      const outputParameters = getOutputParameters(element);
       expect(outputParameters).to.exist;
       expect(outputParameters.length).to.equal(1);
 
-      const inputOutput = getInputOutput(shape);
+      const inputOutput = getInputOutput(element);
       expect(inputOutput).to.exist;
     }));
 
 
-    it('should undo/redo', inject(function(eventBus) {
+    it('should undo/redo', inject(function(commandStack) {
 
       // when
-      eventBus.fire('commandStack.properties-panel.update-businessobject.reverted', context);
-      eventBus.fire('commandStack.properties-panel.update-businessobject.executed', context);
+      commandStack.undo();
+      commandStack.redo();
 
       // then
-      const inputOutput = getInputOutput(shape);
+      const inputOutput = getInputOutput(element);
       expect(inputOutput).to.not.exist;
     }));
 
@@ -161,27 +140,35 @@ describe('camunda-cloud/features/modeling - update propagateAllChildVariables at
 
   describe('set propagateAllChildVariables to false', function() {
 
-    let shape, calledElement, context;
+    let element, calledElement;
 
-    beforeEach(inject(function(eventBus, elementRegistry, elementFactory) {
+    beforeEach(inject(function(bpmnFactory, commandStack, elementRegistry) {
 
       // given
-      shape = elementRegistry.get('CallActivity_7');
-      calledElement = getCalledElement(shape);
+      element = elementRegistry.get('CallActivity_7');
 
-      context = {
-        context: {
-          element: shape,
-          objectsToAdd: [ elementFactory.createShape({ type: 'zeebe:Output' }) ]
-        }
-      };
+      const businessObject = getBusinessObject(element);
 
-      // assume
-      const inputOutput = getInputOutput(shape);
-      expect(inputOutput).to.not.exist;
+      const extensionElements = businessObject.get('extensionElements');
+
+      const ioMapping = bpmnFactory.create('zeebe:IoMapping');
+
+      calledElement = getCalledElement(element);
 
       // when
-      eventBus.fire('commandStack.properties-panel.update-businessobject-list.executed', context);
+      commandStack.execute('properties-panel.update-businessobject-list', {
+        element: element,
+        currentObject: extensionElements,
+        propertyName: 'values',
+        objectsToAdd: [ ioMapping ]
+      });
+
+      commandStack.execute('properties-panel.update-businessobject-list', {
+        element: element,
+        currentObject: ioMapping,
+        propertyName: 'outputParameters',
+        objectsToAdd: [ bpmnFactory.create('zeebe:Output') ]
+      });
     }));
 
 
@@ -192,21 +179,21 @@ describe('camunda-cloud/features/modeling - update propagateAllChildVariables at
     }));
 
 
-    it('should undo', inject(function(eventBus) {
+    it('should undo', inject(function(commandStack) {
 
       // when
-      eventBus.fire('commandStack.properties-panel.update-businessobject-list.reverted', context);
+      commandStack.undo();
 
       // assume
       expect(calledElement.propagateAllChildVariables).to.equal(true);
     }));
 
 
-    it('should undo/redo', inject(function(eventBus) {
+    it('should undo/redo', inject(function(commandStack) {
 
       // when
-      eventBus.fire('commandStack.properties-panel.update-businessobject-list.reverted', context);
-      eventBus.fire('commandStack.properties-panel.update-businessobject-list.executed', context);
+      commandStack.undo();
+      commandStack.redo();
 
       // then
       expect(calledElement.propagateAllChildVariables).to.equal(false);
