e7c7efd89d118554a49dddf34342a33891f831b7
Niklas Kiefer <niklas.kiefer@camunda.com>
2022-02-18 09:31:44 +0100

    feat: add cloud element templates validator util

---
+++ test/camunda-cloud/ElementTemplatesValidatorSpec.js
@@ -0,0 +1,23 @@
+import { CloudElementTemplatesValidator } from 'lib/camunda-cloud/ElementTemplatesValidator';
+
+
+describe('camunda-cloud - ElementTemplatesValidator', function() {
+
+  [
+    'addAll',
+    'add',
+    'getErrors',
+    'getValidTemplates',
+    'isSchemaValid',
+  ].forEach(function(method) {
+    it(`should export ${method}`, function() {
+
+      // given
+      const validator = new CloudElementTemplatesValidator();
+
+      // then
+      expect(validator[method]).to.exist;
+    });
+  });
+
+});
