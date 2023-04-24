06947ab4a349a19326d75908372dc02274dfcb4d
Philipp <philippfromme@outlook.com>
2023-04-24 14:52:46 +0200

    test: test viewers and modelers

---
+++ lib/camunda-platform/NavigatedViewer.spec.ts
@@ -0,0 +1,14 @@
+import NavigatedViewer from "./NavigatedViewer";
+
+import { testViewer } from "./Viewer.spec";
+
+const viewer = new NavigatedViewer({
+  container: document.createElement('div'),
+  position: 'absolute',
+  width: 100,
+  height: 100,
+  additionalModules: [],
+  moddleExtensions: {}
+});
+
+testViewer(viewer);
