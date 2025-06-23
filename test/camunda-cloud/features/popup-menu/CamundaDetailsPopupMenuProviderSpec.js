5d90673d72fa9505b5aeba64a42f8ae8d5696fa7
Simon <simon.steinruecken@camunda.com>
2025-06-23 13:42:00 +0200

    fix: rank service task higher than other job workers in search
    Closes https://github.com/camunda/camunda-bpmn-js/issues/401
    
    ---------
    
    Co-authored-by: Nico Rehwaldt <nico.rehwaldt@camunda.com>

--- test/camunda-cloud/features/popup-menu/CamundaDetailsPopupMenuProviderSpec.js
+++ test/camunda-cloud/features/popup-menu/CamundaDetailsPopupMenuProviderSpec.js
@@ -1,4 +1,18 @@
 import {
+  act,
+  waitFor
+} from '@testing-library/preact';
+
+import {
+  query as domQuery,
+  queryAll as domQueryAll
+} from 'min-dom';
+
+import {
+  map
+} from 'min-dash';
+
+import {
   bootstrapModeler,
   inject,
   getBpmnJS
@@ -137,19 +151,110 @@ describe('camunda-cloud/features/popup-menu - CamundaDetailsPopupMenuProvider', 
 
   });
 
+
+  it('should rank create-service-task higher than other job workers', inject(async function(canvas) {
+
+    // given
+    const rootElement = canvas.getRootElement();
+
+    // when
+    openPopup(rootElement, 'bpmn-create', { search: true });
+
+    await triggerSearch('job');
+
+    await expectEntries([
+      'create-service-task',
+      'create-send-task',
+      'create-script-task',
+      'create-message-end',
+      'create-message-intermediate-throw'
+    ]);
+
+  }));
+
 });
 
 
 // helpers //////////////
 
-function openPopup(element, providerId) {
+function openPopup(element, providerId, options) {
 
   return getBpmnJS().invoke(function(popupMenu) {
 
-    popupMenu.open(element, providerId, { x: 100, y: 100 });
+    popupMenu.open(element, providerId, { x: 100, y: 100 }, options);
 
     return popupMenu._current;
   });
+}
+
+function getPopupContainer() {
+  return getBpmnJS().invoke(function(popupMenu) {
+    const current = popupMenu._current;
+
+    expect(current, 'expect popupMenu to be open').to.exist;
+
+    return current.container;
+  });
+}
+
+function queryPopup(selector, q = domQuery) {
+  var container = getPopupContainer();
+
+  if (selector) {
+    expect(container).to.exist;
+
+    return q(selector, container);
+  }
+
+  return container;
+}
+
+function queryPopupAll(selector) {
+  return queryPopup(selector, domQueryAll);
+}
+
+/**
+ * @param {string} key
+ *
+ * @return {KeyboardEvent}
+ */
+function keyUp(key) {
+  return new KeyboardEvent('keyup', { key, bubbles: true });
+}
+
+function triggerSearch(value) {
+
+  return act(() => {
+    var searchInput = queryPopup('.djs-popup-search input');
+
+    expect(searchInput, 'search exists').to.exist;
+
+    searchInput.value = value;
+    searchInput.dispatchEvent(keyUp('ArrowRight'));
+  });
+}
+
+/**
+ * Return ids of currently open popup menu entries.
+ *
+ * @return {Promise<string[]>} entryIds
+ */
+function queryEntryIds() {
+  return waitFor(() => {
+    return queryPopupAll('.entry');
+  }).then(entries => {
+    return map(entries, e => e.dataset.id);
+  });
+}
+
+/**
+ * @param {string[]} expectedEntryIds
+ */
+async function expectEntries(expectedEntryIds) {
+
+  const entryIds = await queryEntryIds();
+
+  expect(entryIds, 'entry ids').to.eql(expectedEntryIds);
 }
 
 function expectAnnotated(entries, expectedAnnotatedIds) {
