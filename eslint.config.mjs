7764ed6bc799407a2b55bbe58303e540774f1537
Philipp Fromme <philipp.fromme@camunda.com>
2026-01-14 14:18:58 +0100

    chore: fix license headers and lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,11 @@
 import bpmnIoPlugin from 'eslint-plugin-bpmn-io';
+import camundaLicensedPlugin from 'eslint-plugin-camunda-licensed';
+
 
 const files = {
+  sources: [
+    'lib/**/*.js',
+  ],
   build: [
     'tasks/**/*.mjs',
     '*.js',
@@ -20,6 +25,12 @@ export default [
   {
     'ignores': files.ignored
   },
+  ...camundaLicensedPlugin.configs.mit.map((config) => {
+    return {
+      ...config,
+      files: files.sources
+    };
+  }),
   ...bpmnIoPlugin.configs.browser.map(config => {
 
     return {
